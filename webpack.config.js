c8900b87753cf9ba0d5dd8416b0ea34f3b63d5cf
yorsyboy <adekunletaiwo43@yahoo.com>
2022-05-03 09:10:32 +0100

    deployment

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,13 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
   output: {
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
@@ -16,11 +22,6 @@ module.exports = {
       },
     ],
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-  ],
   devServer: {
     static: './dist',
   },
